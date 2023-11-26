6302824787f0999de228ee7d4790457af4456367
Hugo Silva <hugo.garcia@rethink.dev>
2023-11-26 12:49:47 -0300

    color+

---
+++ Source/SwiftUI/Extensions/Color+.swift
@@ -0,0 +1,14 @@
+//
+//  Color+.swift
+//  personalEncyclopedia
+//
+//  Created by Hugo Silva on 26/11/23.
+//
+
+import SwiftUI
+
+extension Color {
+    static var themeLightBlue: Color {
+        return Color("ThemeLightBlue")
+    }
+}
