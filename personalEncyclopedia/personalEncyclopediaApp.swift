f601c6f0c0659f26e55f3a02d5f3dc5e70bb6ebf
Hugo Silva <hugo.garcia@rethink.dev>
2023-11-26 12:03:15 -0300

    first

---
+++ personalEncyclopedia/personalEncyclopediaApp.swift
@@ -0,0 +1,17 @@
+//
+//  personalEncyclopediaApp.swift
+//  personalEncyclopedia
+//
+//  Created by Hugo Silva on 26/11/23.
+//
+
+import SwiftUI
+
+@main
+struct personalEncyclopediaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
