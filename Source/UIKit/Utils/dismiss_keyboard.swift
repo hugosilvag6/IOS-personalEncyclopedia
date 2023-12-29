1a29ebd2f671e1c6e5604cdc8f45c106b54ba60d
Hugo Silva <hugo.garcia@rethink.dev>
2023-12-28 22:18:28 -0300

    uikit dismiss keyboard

---
+++ Source/UIKit/Utils/dismiss_keyboard.swift
@@ -0,0 +1,25 @@
+//
+//  dismiss_keyboard.swift
+//  personalEncyclopedia
+//
+//  Created by Hugo Silva on 28/12/23.
+//
+
+import UIKit
+
+class DismissKeyboardViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        let tap = UITapGestureRecognizer(target: self, action: #selector(dismissKeyboard))
+        view.addGestureRecognizer(tap)
+    }
+    
+    @objc func dismissKeyboard() {
+        self.view.endEditing(true)
+    }
+}
+
